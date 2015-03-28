233240cb1d13acddaadd313c1c4a29fae0c636e6
Bernd Löffeld <Bernd.Loeffeld@magicinternet.de>
2015-03-28 21:08:55 +0100

    Add thymeleaf

---
+++ src/main/java/de/aquariumshow/application/WebMvcConfiguration.java
@@ -0,0 +1,15 @@
+package de.aquariumshow.application;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+	@Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}
--- src/test/java/de/aquariumshow/controller/AquariumControllerTest.java
+++ src/test/java/de/aquariumshow/controller/AquariumControllerTest.java
@@ -14,9 +14,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.aquariumshow.application.Application;
 import de.aquariumshow.model.Aquarium;
